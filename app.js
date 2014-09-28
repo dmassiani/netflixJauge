c597bebf9d9ce6c10ce54039580dc25b85669c0f
Massiani David <davidmassianirennes@gmail.com>
2014-09-28 20:26:03 +0200

    add update and kill jauge

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
+// on vérifie que du local existe
 var aujourdhui = new Date();
-var newJauge = '"' + aujourdhui.getYear() + ',' + aujourdhui.getMonth() + ',' + aujourdhui.getDay() + '"';
 
-	if( simpleStorage.get("laura") === undefined ){
-		simpleStorage.set("laura", {"jauge":0, "update": newJauge});
-	}
-	var Laura = simpleStorage.get('Laura');
+if( simpleStorage.get("laura") === undefined ){
+	simpleStorage.set("laura", {"jauge":0, "update": aujourdhui});
+}
+var Laura = simpleStorage.get('laura');
 
-	if( simpleStorage.get("emma") === undefined ){
-		simpleStorage.set("emma", {"jauge":0, "update": newJauge});
-	}
-	var Emma = simpleStorage.get('Emma');
+if( simpleStorage.get("emma") === undefined ){
+	simpleStorage.set("emma", {"jauge":0, "update": aujourdhui});
+}
+var Emma = simpleStorage.get('emma');
 
 
 function diffdate(d1,d2){
@@ -20,9 +20,33 @@ function diffdate(d1,d2){
 
 $(document).ready(function(){
 
-
 	var dateJauge = new Date(Laura.update);
- 
-	alert(diffdate(dateJauge,aujourdhui));
+	// var update = diffdate(dateJauge,aujourdhui);
+
+	if( dateJauge.getDay() != aujourdhui.getDay() ){
+		// on supprime de la jauge le nombre de jours de diff
+		var diff = diffdate( dateJauge,aujourdhui );
+
+		if( 0 > ( Laura.jauge - diff )){
+			// la jauge serait inférieur à 0 donc mise à zéro de la jauge
+			simpleStorage.set("laura", {"jauge":0, "update": aujourdhui});
+		}else{
+			simpleStorage.set("laura", {"jauge": Laura.jauge - diff, "update": aujourdhui});		
+		}
+		Laura = simpleStorage.get('laura');
+
+		if( 0 > ( Emma.jauge - diff )){
+			// la jauge serait inférieur à 0 donc mise à zéro de la jauge
+			simpleStorage.set("emma", {"jauge":0, "update": aujourdhui});
+		}else{
+			simpleStorage.set("emma", {"jauge": Emma.jauge - diff, "update": aujourdhui});		
+		}
+		Emma = simpleStorage.get('emma');
+
+
+	}else{
+		console.log('no update');
+		
+	}
 
 });
